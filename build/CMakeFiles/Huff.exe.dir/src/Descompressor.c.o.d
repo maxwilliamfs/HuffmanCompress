3fc5e9a9957f5127cd4f88e972fe9f52b80707ff
Max William Falcão Santos <williammaxbcc4794@gmail.com>
2026-05-28 14:18:43 -0300

    docs : atualiza gitignore

--- build/CMakeFiles/Huff.exe.dir/src/Descompressor.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Huff.exe.dir/src/Descompressor.c.o: \
- /home/maxwilliamfalcaosantos/Documentos/GitHub/HuffmanCompress/src/Descompressor.c \
- /usr/include/stdc-predef.h \
- /home/maxwilliamfalcaosantos/Documentos/GitHub/HuffmanCompress/src/Descompressor.h
